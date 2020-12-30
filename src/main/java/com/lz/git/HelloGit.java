9bd33c11e0569873972c67491ef11d57f15b8893
LZ-00 <1097548250@qq.com>
2020-12-30 12:02:57 +0800

    更新1

---
+++ src/main/java/com/lz/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.lz.git;
+
+/**
+ * @author 乐。
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+        System.out.println("更新1");
+    }
+
+}
