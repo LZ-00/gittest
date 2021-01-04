b92c0d64757e788eae9f36fa34255ea2d217c37a
lz <64174046+LZ-00@users.noreply.github.com>
2021-01-04 13:21:39 +0800

    delete

--- src/main/java/com/lz/git/HelloGit.java
+++ src/main/java/com/lz/git/HelloGit.java
@@ -14,7 +14,6 @@ public class HelloGit {
         System.out.println("主干and分支");
         System.out.println("更新to本地库");
         System.out.println("test fetch");
-        System.out.println("更新");
     }
 
 }
