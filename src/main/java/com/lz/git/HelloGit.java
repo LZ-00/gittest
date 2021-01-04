47ef389d84f47e3a8228f1186675732a632204f1
lz <64174046+LZ-00@users.noreply.github.com>
2021-01-04 13:12:53 +0800

    add branch

--- src/main/java/com/lz/git/HelloGit.java
+++ src/main/java/com/lz/git/HelloGit.java
@@ -6,7 +6,7 @@ package com.lz.git;
 public class HelloGit {
 
     public static void main(String[] args) {
-        System.out.println("hello Git");
+        System.out.println("hello Git (branch)");
         System.out.println("更新1");
         System.out.println("更新2");
         System.out.println("更新3");
